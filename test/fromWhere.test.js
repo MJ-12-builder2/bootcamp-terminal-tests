55f268a47a10da5535b6c00b11e5c201ce0639db
MJ-12-builder2 <m_ngwekazi@yahoo.com>
2021-09-18 23:51:58 +0200

    amended assert.equal

--- test/fromWhere.test.js
+++ test/fromWhere.test.js
@@ -4,15 +4,9 @@ let fromWhere = require("../fromWhere");
 describe('The fromWhere function', function(){
 
     it('should return "fromWhere" after checking regNumber', function(){
-      assert.equal('regNumber CY, Bellville', fromWhere('Bellville'));
-    });
-    it('should return "fromWhere" after checking regNumber', function(){
-        assert.equal('regNumber(CJ), Paarl',fromWhere('Paarl'));
-    });
-    it('should return "fromWhere" after checking regNumber', function(){  
-        assert.equal('regNumber(CA), Cape Town', fromWhere ('Cape Town'));
-    });
-    it('should return "fromWhere" after checking regNumber', function(){
-        assert.equal('regNumber(CC), Some other place!', fromWhere ('Some other place!'));        
+        assert.equal(fromWhere('CY'), 'Bellville');
+        assert.equal(fromWhere('CJ'), 'Paarl');
+        assert.equal(fromWhere('CA'), 'Cape Town');
+        assert.equal(fromWhere('CC'), 'Some other place!');        
     });
     })
